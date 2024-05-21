22cf8dd8d9ce14cf0b4fb95b7566a312b72ced2b
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-21 18:31:33 -0500

    building endpoints

--- server/routes/form.router.js
+++ server/routes/form.router.js
@@ -1,18 +1,97 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // GET route code here
+});
+//Price Ranges
+router.get("/price-ranges", (req, res) => {
+  sqlText = `
+    SELECT * FROM "price_ranges"
+    `;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.error("Error fetching Price Ranges", error);
+    });
+});
+
+//Meat Preference
+router.get("/meat-preferences", (req, res) => {
+  sqlText = `
+    SELECT * FROM "meat_preferences"
+    `;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.error("Error fetching Meat Preference", error);
+    });
+});
+
+//religious restriction options
+router.get("/religious-options", (req, res) => {
+  sqlText = `
+    SELECT * FROM "religious_restrictions"
+    `;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.error("Error fetching Religious options", error);
+    });
+});
+
+//allergen options
+router.get("/allergen-options", (req, res) => {
+  sqlText = `
+    SELECT * FROM "allergens"
+    `;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.error("Error fetching allergens list", error);
+    });
+});
+
+//cuisine options
+router.get("/cuisine-options", (req, res) => {
+  sqlText = `
+    SELECT * FROM "cuisine_types"
+    `;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.error("Error fetching cuisines", error);
+    });
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
 });
 

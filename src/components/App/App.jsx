0ea758575eb10776a3971a9d27bf2ecf361765f1
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-21 14:46:26 -0500

    create files for Preferences page and form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import PreferencesPage from '../PreferencesPage/PreferencesPage'
 
 import './App.css';
 
@@ -66,6 +67,10 @@ function App() {
             path="/info"
           >
             <InfoPage />
+          </ProtectedRoute>
+          
+          <ProtectedRoute exact path="/preferences">
+            <PreferencesPage />
           </ProtectedRoute>
 
           <Route

d1378c909d69bfe4bc51d1026c5e2a89e9121153
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-22 14:32:59 -0500

    add "update Preferences" button

--- server/routes/userPreferences.router.js
+++ server/routes/userPreferences.router.js
@@ -2,13 +2,53 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
-router.get("/", async(req, res)=>{
-  const sqlText = `
-  SELECT * 
-  FROM "user_preferences", "user_allergens"
-  JOIN user_allergens on user_ID
-  `
-})
+
+
+//GET single user's preferences
+router.get("/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const client = await pool.connect();
+
+  try {
+    // Fetch user preferences
+    const userPreferencesQuery = `
+      SELECT
+        up.id,
+        up.user_id,
+        pr.range AS max_price_range,
+        mp.preference AS meat_preference,
+        rr.restriction AS religious_restrictions,
+        up.cuisine_types,
+        up.max_distance,
+        up.open_now,
+        up.accepts_large_parties
+      FROM user_preferences up
+      LEFT JOIN price_ranges pr ON up.max_price_range = pr.id
+      LEFT JOIN meat_preferences mp ON up.meat_preference = mp.id
+      LEFT JOIN religious_restrictions rr ON up.religious_restrictions = rr.id
+      WHERE up.user_id = $1;
+    `;
+    const userPreferencesResult = await client.query(userPreferencesQuery, [userId]);
+    const userPreferences = userPreferencesResult.rows[0];
+
+    // Fetch user allergens
+    const allergenQuery = `
+      SELECT a.id, a.allergen
+      FROM user_allergens ua
+      JOIN allergens a ON ua.allergen_id = a.id
+      WHERE ua.user_id = $1;
+    `;
+    const allergenResult = await client.query(allergenQuery, [userId]);
+    const allergens = allergenResult.rows;
+
+    res.json({ ...userPreferences, allergens });
+  } catch (error) {
+    console.error("Error fetching user preferences:", error);
+    res.status(500).send("Error fetching user preferences");
+  } finally {
+    client.release();
+  }
+});
 
 router.post("/", async (req, res) => {
   const client = await pool.connect();
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { Button } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+
+  const updatePrefs = () => {
+    history.push("/preferences");
+  };
+
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <h3>Preferences Summary</h3>
+      <Button variant='contained' color="primary" onClick={updatePrefs}>
+        Update Preferences
+      </Button>
       <LogOutButton className="btn" />
     </div>
   );

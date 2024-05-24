f41ed9fb41e1528b1b0ec987983e9a17ded707c1
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-23 20:01:52 -0500

    updating google maps component

--- src/components/MapPlaceholder/MapPlaceholder.jsx
+++ src/components/MapPlaceholder/MapPlaceholder.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import './MapPlaceholder.css';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 const MapPlaceholder = () => {
   return (
-    <div className="map-placeholder">
-      <h3>Map Coming Soon!</h3>
-      <p>We're working on integrating a Google Maps component to enhance your experience.</p>
-    </div>
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        {/* You can add markers or other components here */}
+      </GoogleMap>
+    </LoadScript>
   );
 };
 

bb48169fd0872eb704906178c92d65240b2cea62
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-31 10:31:37 -0500

    tweak landing page for legibility

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -22,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.common.white,
     textAlign: "center",
   },
+  body1: {
+    fontSize: '1.2rem',
+  },
 }));
 
 function LandingPage() {
@@ -46,14 +49,14 @@ function LandingPage() {
               No More Hangry Meltdowns!
             </Typography>
             <br />
-            <Typography variant="body">
+            <Typography variant="body1">
               Are you tired of the endless debates and indecision when it comes
               to choosing a place to eat with your friends, family, or
               colleagues? Say goodbye to the hassle and hello to SupprThyme -
               the mobile web application that revolutionizes the way you dine
               out!
             </Typography>
-            <Typography variant="body">
+            <Typography variant="body1">
               SupprThyme transforms the often-frustrating experience of reaching
               a consensus on a dining destination into an enjoyable and gamified
               culinary adventure. We all have different tastes, budgets, and
@@ -64,19 +67,19 @@ function LandingPage() {
             <Typography variant="h3" component="h2">
               How does it work?
             </Typography>
-            <Typography variant="body">
+            <Typography variant="body1">
               It's simple! Someone creates a group and invites the others. Our
               innovative app then combines everyone in the group's preferences
               and searches our extensive restaurant database to suggest local
               dining options that meet your criteria.
             </Typography>
-            <Typography variant="body">
+            <Typography variant="body1">
               With SupprThyme, decision-making becomes a fun and interactive
               experience. Users can vote on the restaurant suggestions, engaging
               in a collaborative process that eventually leads to a unanimous
               choice.
             </Typography>
-            <Typography variant="body">
+            <Typography variant="body1">
               Join us on this gastronomic journey and let SupprThyme take the
               stress out of choosing where to eat. Say goodbye to indecision and
               hello to delightful dining experiences with SupprThyme!
